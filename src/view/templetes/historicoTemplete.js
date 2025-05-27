456a9a4a40e86af268e2b392c1fbdf5e700b1c3a
seu nome de usuário <pealq63@gmail.com>
2025-05-27 14:04:50 -0300

    colocado verificação para o objeto sembre ser um objeto

--- src/view/templetes/historicoTemplete.js
+++ src/view/templetes/historicoTemplete.js
@@ -1,8 +1,16 @@
+import { modal } from "../../utilidades/modal.js";
+
 export function gerarTempletesDosCalculosRealizados(valores){
    return valores.map(valor => {
             // Gera os templates dos cálculos realizados, adaptando-se dinamicamente à quantidade de valores no objeto de resposta
             console.log(valores)
-            const camposResposta = Object.entries(valor.resposta).map(([chave, valorCampo]) => {
+            const resposta = valor.resposta || {};
+
+            if (!valor.resposta) {
+                modal.mostrarDialog("Objeto sem resposta:", valor);
+            }
+
+            const camposResposta = Object.entries(resposta).map(([chave, valorCampo]) => {
                 return `<tr>
                             <td>${chave}</td>  
                             <td>${valorCampo}</td>
